a129c23b78fe8ffe670f122c8de3320f2bbb43aa
karpik1212 <kirillkarpenko1203@gmail.com>
2024-10-27 20:20:38 +0200

    auth

---
+++ app/auth.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+connection = sqlite3.connect('rent.db')
+cursor = connection.cursor()
+
+def insert_users(username, email, number, password):
+    cursor.execute('INSERT INTO users (username, email, number, password) VALUES (?, ?, ?, ?)',
+                   (username, email, number, password))
+    return cursor.lastrowid
+
+user_id = insert_users('test_name', 'test@gmail.com', '+380759910319', 'test123pass')
+connection.commit()
+connection.close()
