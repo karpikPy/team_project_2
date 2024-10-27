77d466a158c9c67c7f5115b0c4554089329e37b1
karpik1212 <kirillkarpenko1203@gmail.com>
2024-10-27 20:16:41 +0200

    .

--- database/__init__.py
+++ database/__init__.py
@@ -1,19 +1,19 @@
 import sqlite3
+def create_db():
+    with sqlite3.connect('rent.db') as db:
+        cursor = db.cursor()
+        cursor.execute("""CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY NOT NULL,
+        username VARCHAR(255),
+        email VARCHAR(255),
+        number VARCHAR(255),
+        password VARCHAR(255));
+        """)
 
-with sqlite3.connect('rent.db') as db:
-    cursor = db.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY NOT NULL,
-    username VARCHAR(255),
-    email VARCHAR(255),
-    number VARCHAR(255),
-    password VARCHAR(255));
-    """)
-
-    cursor.execute("""CREATE TABLE IF NOT EXISTS houses (
-    id INTEGER PRIMARY KEY NOT NULL,
-    name VARCHAR(255),
-    place VARCHAR(255),
-    price INTEGER NOT NULL);   
-    """)
+        cursor.execute("""CREATE TABLE IF NOT EXISTS houses (
+        id INTEGER PRIMARY KEY NOT NULL,
+        name VARCHAR(255),
+        place VARCHAR(255),
+        price INTEGER NOT NULL);   
+        """)
 
