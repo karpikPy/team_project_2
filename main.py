a30bbd94608f14f20617aaf706ee387b9815900d
mollygin <yevartemivna.rul@gmail.com>
2024-10-29 22:26:37 +0100

    routes and main modified

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from flask import (Flask, render_template, request)
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template("index.html")
+from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True, port=1212)
+    app.run(debug=True, port=7644)
