44f3b42b616ae90cab302100d65be8fb8190849d
karpikPy <karpikxd1212@gmail.com>
2024-11-02 20:42:05 +0200

    Update auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,5 @@
 import sqlite3
 
-# Establish a connection to the database
 connection = sqlite3.connect('../database/rent.db')
 cursor = connection.cursor()
 
@@ -15,13 +14,11 @@ def insert_users(username, email, number, password):
 
 def insert_house(name, place, price, people, animals, image_path=None):
     try:
-        # Set image_data to None by default
         image_data = None
         if image_path:
             with open(image_path, 'rb') as file:
                 image_data = file.read()
 
-        # Insert house details, including people and animals fields
         cursor.execute('INSERT INTO houses (name, place, price, people, animals, image) VALUES (?, ?, ?, ?, ?, ?)',
                        (name, place, price, people, animals, image_data))
         return cursor.lastrowid
