e0b2bcd31c3ed2de7263e4212850fc44affd6980
maksym <maxgamesda@gmail.com>
2024-11-10 16:13:16 -0200

    resolve conflicts

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("login.html")
     houses = process.houses()
     return render_template("index.html", houses=houses)
 
